427c284dc0f2fd0eb853b6142d34d2fd7fa1c830
vigneshshankar1998 <47172568+vigneshshankar1998@users.noreply.github.com>
2019-02-10 21:32:59 +0530

    Update mul.py

--- mul.py
+++ mul.py
@@ -1,5 +1,5 @@
-def printMultiples(n, m):
-for m in (n,m):
-    if n % 2 == 0:
-        while n < 0:
-            print(n)
+def printMultiples(num, m):
+for m in (num,m):
+    if num % 2 == 0:
+        while num < 0:
+            print(num)
