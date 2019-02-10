8fb6cf1897bf924459762f3ffd67477ab5af637a
vigneshshankar1998 <47172568+vigneshshankar1998@users.noreply.github.com>
2019-02-10 21:13:12 +0530

    Create eveninteval.py

---
+++ eveninteval.py
@@ -0,0 +1,10 @@
+# Python program to print Even Numbers in given range 
+
+start, end 
+
+# iterating each number in list 
+for n in range(start + 1, end + 1): 
+	
+	# checking condition 
+	if n % 2 == 0: 
+		print(n, end = " ") 
