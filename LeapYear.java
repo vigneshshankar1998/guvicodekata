8f464434474c45c3b1040a9b25f31f8d894375c1
vigneshshankar1998 <47172568+vigneshshankar1998@users.noreply.github.com>
2019-02-10 20:05:58 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -7,15 +7,6 @@ public class LeapYear {
 
         if(year % 4 == 0)
         {
-            if( year % 100 == 0)
-            {
-                // year is divisible by 400, hence the year is a leap year
-                if ( year % 400 == 0)
-                    leap = true;
-                else
-                    leap = false;
-            }
-            else
                 leap = true;
         }
         else
