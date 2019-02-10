8266cc9255ab1a8b02e0d4b92a4527043d6519fb
vigneshshankar1998 <47172568+vigneshshankar1998@users.noreply.github.com>
2019-02-10 20:00:47 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,29 @@
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        int;
+        boolean leap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                // year is divisible by 400, hence the year is a leap year
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
