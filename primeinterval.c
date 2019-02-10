87c819e013f51e17a3036b90b881410c8b7f5714
vigneshshankar1998 <47172568+vigneshshankar1998@users.noreply.github.com>
2019-02-10 21:07:04 +0530

    Create primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int l, h, i, flag;
+    scanf("%d %d", &l, &h);
+
+    printf( l, h);
+
+    while (l < h)
+    {
+        flag = 0;
+
+        for(i = 2; i <= l/2; ++i)
+        {
+            if(l % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", l);
+
+        ++l;
+    }
+
+    return 0;
+}
