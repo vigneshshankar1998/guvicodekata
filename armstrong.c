a47c393cd4b1068d1f16637eb43649357ea695e2
vigneshshankar1998 <47172568+vigneshshankar1998@users.noreply.github.com>
2019-02-10 21:19:18 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n, originalNumber, remainder, result = 0;
+    scanf("%d", &n);
+
+    originalNumber = n;
+
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        result += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+
+    if(result == n)
+        printf("yes");
+    else
+        printf("n");
+
+    return 0;
+}
